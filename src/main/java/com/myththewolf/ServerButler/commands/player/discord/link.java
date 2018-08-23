0a977288af8cd61934324c55455db101460f35bd
MythTheWolf <myth.wolf6@gmail.com>
2018-08-22 23:13:25 -0500

    Added discord-linking commands

---
+++ src/main/java/com/myththewolf/ServerButler/commands/player/discord/link.java
@@ -0,0 +1,44 @@
+package com.myththewolf.ServerButler.commands.player.discord;
+
+import com.myththewolf.ServerButler.lib.cache.DataCache;
+import com.myththewolf.ServerButler.lib.command.impl.DiscordCommandAdapter;
+import com.myththewolf.ServerButler.lib.mySQL.SQLAble;
+import com.myththewolf.ServerButler.lib.player.interfaces.MythPlayer;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.server.Server;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class link extends DiscordCommandAdapter implements SQLAble {
+    @Override
+    public void onCommand(Message orgin, TextChannel channel, Server server, Optional<MythPlayer> sender, String[] args) {
+        if (args.length < 1) {
+            reply("**Usage:** ;link <token>");
+        }
+        if (sender.isPresent()) {
+            reply(":x: You already are linked!");
+            return;
+        }
+        reply(":mag: Searching database...");
+        ResultSet resultSet = prepareAndExecuteSelectExceptionally("SELECT * FROM `SB_Discord` WHERE `token` = ?", 1, args[0]);
+        try {
+            if (resultSet.next()) {
+                String UUID = resultSet.getString("UUID");
+                MythPlayer mp = DataCache.getOrMakePlayer(UUID);
+                reply(":white_check_mark: Hello," + mp.getName() + "! (UUID: " + mp.getUUID() + ")");
+                reply(":warning: Please Re-Join the minecraft server in order to re-fresh discord permissions.");
+                mp.setDiscordID(orgin.getAuthor().getIdAsString());
+                mp.updatePlayer();
+                return;
+            } else {
+                reply(":warning: Invalid token! (0 Results)");
+                return;
+            }
+        } catch (SQLException e) {
+            reply(":bomb: **ERROR:** " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/myththewolf/ServerButler/commands/player/token.java
@@ -0,0 +1,33 @@
+package com.myththewolf.ServerButler.commands.player;
+
+import com.myththewolf.ServerButler.lib.MythUtils.StringUtils;
+import com.myththewolf.ServerButler.lib.command.impl.CommandAdapter;
+import com.myththewolf.ServerButler.lib.config.ConfigProperties;
+import com.myththewolf.ServerButler.lib.mySQL.SQLAble;
+import com.myththewolf.ServerButler.lib.player.interfaces.MythPlayer;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Optional;
+
+public class token extends CommandAdapter implements SQLAble {
+    @Override
+    public void onCommand(Optional<MythPlayer> sender, String[] args, JavaPlugin javaPlugin) {
+        if (!sender.isPresent()) {
+            return;
+        }
+
+        MythPlayer player = sender.get();
+        if (player.getDiscordID().isPresent()) {
+            reply(ConfigProperties.PREFIX + ChatColor.RED + "You are already linked!");
+            return;
+        }
+        reply(ConfigProperties.PREFIX + "Generating key...");
+        String key = StringUtils.getToken(10);
+        reply(ConfigProperties.PREFIX + "Done! PM The token bot with this command: ;" + "link " + key);
+
+        prepareAndExecuteUpdateExceptionally("INSERT INTO `SB_Discord` (`token`,`UUID`) VALUES (?,?)", 2, key, player
+                .getUUID());
+    }
+
+}
